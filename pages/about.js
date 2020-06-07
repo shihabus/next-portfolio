efceb6236eba85cf41adae5993e3391bb62c0e9a
Shihab US <shihabudheen@bounceshare.com>
2020-06-07 18:52:12 +0530

    Adding getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,43 @@
+import { Component } from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
+// import fetch from "isomorphic-unfetch";
 
-const About = () => (
-  <Layout title="About">
-    <Link href="/">
-      <a>Go to Home</a>
-    </Link>
-    <div>A JS developer</div>
-    <img src="/static/js-logo.png" alt="js-logo" />
-  </Layout>
-);
+class About extends Component {
+  state = {
+    user: this.props.user,
+  };
+
+  // this runs in server
+  static getInitialProps() {
+    return fetch(`https://api.github.com/users/octocat`)
+      .then((res) => res.json())
+      .then((data) => ({ user: data }));
+  }
+
+  // this runs on client
+  // componentDidMount = () => {
+  //   fetch(`https://api.github.com/users/octocat`)
+  //     .then((res) => res.json())
+  //     .then((data) => this.setState({ user: data }));
+  // };
+
+  render() {
+    return (
+      <Layout title="About">
+        <Link href="/">
+          <a>Go to Home</a>
+        </Link>
+        {JSON.stringify(this.state.user)}
+        <div>A JS developer</div>
+        <img src="/static/js-logo.png" alt="js-logo" />
+      </Layout>
+    );
+  }
+}
+
+// About.getInitialProps = () => {
+
+// }
 
 export default About;
